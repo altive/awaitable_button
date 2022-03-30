536330bb5ec664270757d7b8ee944bfa7a06daf7
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2022-03-31 06:31:56 +0900

    awaitable_button 0.3.1

--- packages/awaitable_button/CHANGELOG.md
+++ packages/awaitable_button/CHANGELOG.md
@@ -1,4 +1,4 @@
-## 0.3.0
+## 0.3.1
 
 - Added `AwaitableOutlinedButton` .
 - Added `onError` to `AwaitableIconButton`.
--- packages/awaitable_button/lib/awaitable_button.dart
+++ packages/awaitable_button/lib/awaitable_button.dart
@@ -2,4 +2,5 @@ library awaitable_button;
 
 export 'src/awaitable_elevated_button.dart';
 export 'src/awaitable_icon_button.dart';
+export 'src/awaitable_outlined_button.dart';
 export 'src/awaitable_text_button.dart';
